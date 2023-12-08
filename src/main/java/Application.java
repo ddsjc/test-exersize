c6afd166187ba433b5e4a26d09518e05663f1720
ddsjc <126328274+ddsjc@users.noreply.github.com>
2023-12-08 23:58:47 +0300

    3d version homevork2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,14 +6,14 @@ public class Application {
         Kotik kotik1 = new Kotik("Barsick", "Myau", 5, 2500);
         Kotik kotik2 = new Kotik();
 
-        kotik2.setName("Vitek");
+        kotik2.setType("Vitek");
         kotik2.setVoice("Miy");
         kotik2.setSatiety(7);
         kotik2.setWeight(3200);
 
         kotik1.liveAnotherDay();
         kotik1.outputSchedule(kotik1.getSchedule());
-        System.out.println("Name is  " + kotik1.getName() + " , weight is " + kotik1.getWeight() + " grams");
+        System.out.println("Name is  " + kotik1.getType() + " , weight is " + kotik1.getWeight() + " grams");
         System.out.println("Count of cats : " + Kotik.getCount());
         if(kotik1.getVoice().equals(kotik2.getVoice())){
             System.out.println("Voices is equals!");
